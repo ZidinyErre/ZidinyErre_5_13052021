3379bc12a15d548f762f18f67b2c7caeb420d8ff
Zidiny Erre <mr.erre.zidiny@gmail.com>
2021-05-18 18:31:21 +0200

    premier échec de création de nav

--- file.js
+++ file.js
@@ -7,7 +7,7 @@ fetch("http://localhost:3000/api/cameras")
 const displayProducts = (products) => {
     products.forEach(product => {
         console.log(product);
-        document.querySelector(".col").innerHTML += "<p> "+ product.imageUrl + "</p>";
+        document.querySelector(".col").innerHTML += "<img> "+ product.imageUrl + "</img>";
         document.querySelector(".col").innerHTML += "<p> "+ product.name + "</p>";
         document.querySelector(".col").innerHTML += "<p> "+ product.price + "</p>";
         document.querySelector(".col").innerHTML += "<p> "+ product.description + "</p>";
