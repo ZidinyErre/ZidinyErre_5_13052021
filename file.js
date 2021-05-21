1ebceb64d3c7843d00c1283f6cbdd8188548a569
Zidiny Erre <mr.erre.zidiny@gmail.com>
2021-05-21 22:09:43 +0200

    essaie mise en place card bootstrap

--- file.js
+++ file.js
@@ -8,6 +8,9 @@ const displayProducts = (products) => {
     products.forEach(product => {
         console.log(product);
         document.querySelector(".col").innerHTML += `<img width="250" src='  ${product.imageUrl}  '>` + `<p>  ${product.name}  </p>` + `<p>   ${product.price/100} euros  </p>` + `<p>  ${product.description}  </p>` ;
+    
     });
 }
 
+let elt = document.querySelector(".card");
+elt.appendChild(displayProducts);
