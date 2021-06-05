3f8ba29df771f89783deeedcc82384011358737f
Zidiny Erre <mr.erre.zidiny@gmail.com>
2021-06-05 17:43:19 +0200

    mise en place de dropdown

--- customization.js
+++ customization.js
@@ -11,30 +11,26 @@ fetch("http://localhost:3000/api/cameras/" + id)
     recover(value);
 });
 
-const recover = (element) => {
-        console.log(element);
-        document.querySelector(".row").innerHTML +=
-        `<div class="col d-flex justify-content-center">
-            <div class="card align-self-start" style="width: 18rem;">
-                <p>  ${element.name}  </p>  
-                <img width="100%" src='  ${element.imageUrl}  '>
-                <div class="card-body">
-                    <p>   ${element.price/100} euros  </p>
-                    <div class="dropdown">
-                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                            Objectif
-                        </button>
-                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">`
-                            const Objectifs = element.lenses;
-                            Objectifs.forEach(item => {
-                                console.log(item);
-                            });
-                            `<li><a class="dropdown-item" href="#"> ${item} </a></li>`
 
-                        `</ul>
-                    </div>  
-                    <p>  ${element.description}  </p>
-                </div>
+
+const recover = (element) => {
+    console.log(element);
+    document.querySelector(".row").innerHTML +=
+    `<div class="col d-flex justify-content-center">
+        <div class="card align-self-start" style="width: 18rem;">
+            <p>  ${element.name}  </p>  
+            <img width="100%" src='  ${element.imageUrl}  '>
+            <div class="card-body">
+                <p>   ${element.price/100} euros  </p>
+                <select class="drop">
+                </select>
+                <p>  ${element.description}  </p>
             </div>
-        </div>`
+        </div>
+    </div>`
+    const x = element.lenses;
+    x.forEach(function(y) 
+        { 
+           document.querySelector(".drop").innerHTML += `<option>${y}</option>` 
+    });
 }
