f805af32843efc9969663bf1ad3c0067b9278306
Zidiny Erre <mr.erre.zidiny@gmail.com>
2021-07-26 20:13:38 +0200

    travail de la sélection des lentilles

--- customization.js
+++ customization.js
@@ -24,34 +24,50 @@ const recover = (element) => {
                 <path d="M4 9.42h1.063C5.4 12.323 7.317 14 10.34 14c.622 0 1.167-.068 1.659-.185v-1.3c-.484.119-1.045.17-1.659.17-2.1 0-3.455-1.198-3.775-3.264h4.017v-.928H6.497v-.936c0-.11 0-.219.008-.329h4.078v-.927H6.618c.388-1.898 1.719-2.985 3.723-2.985.614 0 1.175.05 1.659.177V2.194A6.617 6.617 0 0 0 10.341 2c-2.928 0-4.82 1.569-5.244 4.3H4v.928h1.01v1.265H4v.928z"/>
               </svg>  </p>
                 <select class="drop">
+                <option value="0" >Lentille</option>
                 </select>
                 <p>  ${element.description}  </p>
-                <button type="button" class="btn btn-primary "> <a href="basket.html?${element._id}">Ajouter</a> </button>
+                <button type="button" class="btn btn-primary "> <a >Ajouter</a> </button>
             </div>
         </div>
     </div>`
-
+    // mettre dans l'ancre du bouton href="basket.html?${element._id}"
     
-    document.querySelector(".drop").innerHTML += `<option value="0" >Lentille</option>`
+    // document.querySelector(".drop").innerHTML += `<option value="0" >Lentille</option>`
     element.lenses.forEach(function(lense) { 
         document.querySelector(".drop").innerHTML += `<option value="1">${lense}</option>`
-        
     });
-    console.log();
+    // J'ai fait une constante qui vise le contrôleur d'options 
+    const selectLense = document.querySelector(".drop");
+    console.log(selectLense); 
+    // J'ai fait une constante qui vise la valeur du controleur d'options
+    const choixlense = selectLense.value;
+    console.log(choixlense); 
     
+    let camera = {
+        id: id,
+        lentille: choixlense,
+    };
 
-    
     document.querySelector(".btn").addEventListener('click', function(){
-        let panier = JSON.parse(localStorage.getItem("panier"));
-        if (panier == null) {
-            panier = [];
-        }
-        panier.push(element._id);
-        // const btnSend = document.querySelector("btn-primary");
-        // console.log(btnSend);
-        localStorage.setItem("panier", JSON.stringify(panier));
-
+        console.log(camera);
     });
+
+    // selectLense.addEventListener('click',function (){
+        
+    // });
+
+    
+    
+    // document.querySelector(".btn").addEventListener('click', function(){
+    //     let panier = JSON.parse(localStorage.getItem("panier"));
+    //     if (panier == null) {
+    //         panier = [];
+    //     }
+    //     panier.push(element._id);
+    //     localStorage.setItem("panier", JSON.stringify(panier));
+
+    // });
 }
 
 
