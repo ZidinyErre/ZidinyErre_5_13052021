ef79af471fa782dd743725a80bd3986aa12cee72
Zidiny Erre <mr.erre.zidiny@gmail.com>
2021-08-03 01:19:29 +0200

    Affichage image

--- basket.js
+++ basket.js
@@ -1,20 +1,20 @@
-let notrePanier = localStorage.getItem("basket");
-if (!notrePanier) {
-    alert("Le panier est vide")
-}
-let notrePanierJSON = JSON.parse(notrePanier);
-console.log(notrePanierJSON);
+// let notrePanier = localStorage.getItem("basket");
+// if (!notrePanier) {
+//     alert("Le panier est vide")
+// }
+// let notrePanierJSON = JSON.parse(notrePanier);
 
+let notrePanierJSON = Object.keys(localStorage);
+console.log(notrePanierJSON);
 for (let i = 0; i < notrePanierJSON.length; i++) {
-    const element = notrePanierJSON[i];
+    const appareil = JSON.parse(localStorage.getItem(notrePanierJSON[i]));
     
     const divDeBase = document.querySelector(".data");
 
     let imgPhoto = document.createElement("img");
     imgPhoto.classList.add("photo");
-    let pic = imgPhoto.setAttribute('src',element.img);
+    let pic = imgPhoto.setAttribute('src',appareil.img);
     imgPhoto.innerHTML = pic;
     divDeBase.appendChild(imgPhoto);
-    console.log(e);
 
 }
