57cd41fc94891611af209fedea34b78f5964a3f9
Zidiny Erre <mr.erre.zidiny@gmail.com>
2021-08-02 17:41:28 +0200

    Essaie de mise en place bout plus et moins

--- customization.js
+++ customization.js
@@ -13,12 +13,12 @@ fetch("http://localhost:3000/api/cameras/" + id)
 const recover = (element) => {
     console.log(element);
     document.querySelector(".row").innerHTML +=
-    `<div class="col d-flex justify-content-center">
+    `<div id="resultat" class="col d-flex justify-content-center">
         <div class="card " style="width: 18rem;">
             <p class="align-self-center">  ${element.name}  </p>  
             <img width="100%" src='  ${element.imageUrl}  '>
-            <div class="card-body">
-                <p>   ${element.price/100} <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-currency-euro" viewBox="0 0 16 16">
+            <div class="card-body text-center">
+                <p class="text">   ${element.price/100} <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-currency-euro" viewBox="0 0 16 16">
                 <path d="M4 9.42h1.063C5.4 12.323 7.317 14 10.34 14c.622 0 1.167-.068 1.659-.185v-1.3c-.484.119-1.045.17-1.659.17-2.1 0-3.455-1.198-3.775-3.264h4.017v-.928H6.497v-.936c0-.11 0-.219.008-.329h4.078v-.927H6.618c.388-1.898 1.719-2.985 3.723-2.985.614 0 1.175.05 1.659.177V2.194A6.617 6.617 0 0 0 10.341 2c-2.928 0-4.82 1.569-5.244 4.3H4v.928h1.01v1.265H4v.928z"/>
               </svg>  </p>
                 <select class="drop">
@@ -28,8 +28,7 @@ const recover = (element) => {
             </div>
         </div>
     </div>`
-    console.log(".drop");
-
+// Définition de la premier option de focale
     let select = document.querySelector(".drop");
     let firstOption = document.createElement("option");
     firstOption.setAttribute('disabled', "disabled");
@@ -37,7 +36,7 @@ const recover = (element) => {
     firstOption.setAttribute('selected', "true");
     firstOption.textContent = "Choisissez la focale";
     select.appendChild(firstOption);
-
+// Définition des autres options de focale
     for( i = 0; i<element.lenses.length; i++){
         let optionLenses = document.createElement("option");
         select.appendChild(optionLenses);
@@ -48,6 +47,33 @@ const recover = (element) => {
     // element.lenses.forEach(function(lense) { 
     //     document.querySelector(".drop").innerHTML += `<option value="1">${lense}</option>`
     // });
+
+    // Création des boutons - et +
+    let paragraphe = document.querySelector(".text");
+    let plus = document.createElement("button");
+    plus.classList.add("plus");
+    plus.textContent = "+";
+    paragraphe.appendChild(plus);
+    let moins = document.createElement("button");
+    moins.classList.add("moins");
+    moins.textContent = "-";
+    paragraphe.appendChild(moins);
+    // Action du clique pour les boutons + et -
+    let result = document.querySelector("#resultat")
+     res = result.value;
+    plus.addEventListener('click', function(){
+        if (result >= 1 && result < 20 ) {
+            result++;
+            // document.getElementById('resultat').value = result;
+        }
+    });
+    moins.addEventListener('click', function(){
+        if (result > 1 && result <= 20 ) {
+            result--;
+            // document.getElementById('resultat').value = result;
+        }
+    });
+    console.log(res);
     
 
     document.querySelector(".btn").addEventListener("click", function(){
