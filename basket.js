ccde9c9ee511fa8deb2f9d974ee541897cd8ed01
Zidiny Erre <mr.erre.zidiny@gmail.com>
2021-08-03 01:04:00 +0200

    Essaie d'affichage d'une imaage sur la page panier

--- basket.js
+++ basket.js
@@ -4,3 +4,17 @@ if (!notrePanier) {
 }
 let notrePanierJSON = JSON.parse(notrePanier);
 console.log(notrePanierJSON);
+
+for (let i = 0; i < notrePanierJSON.length; i++) {
+    const element = notrePanierJSON[i];
+    
+    const divDeBase = document.querySelector(".data");
+
+    let imgPhoto = document.createElement("img");
+    imgPhoto.classList.add("photo");
+    let pic = imgPhoto.setAttribute('src',element.img);
+    imgPhoto.innerHTML = pic;
+    divDeBase.appendChild(imgPhoto);
+    console.log(e);
+
+}
