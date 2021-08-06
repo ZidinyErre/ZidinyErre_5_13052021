0b14dc6288e1380d5522cf4af294b68a9cb18f1b
Zidiny Erre <mr.erre.zidiny@gmail.com>
2021-08-06 20:27:06 +0200

    Essaie de changement de position 2

--- customization.js
+++ customization.js
@@ -13,7 +13,7 @@ const recover = (element) => {
     console.log(element);
     // carte Bootstrap
     let card = document.querySelector(".card");
-    
+
     // Création et mise en place de la photo
     let imgAppareil = document.createElement("img");
     imgAppareil.classList.add("imgAppareil");
@@ -63,42 +63,26 @@ const recover = (element) => {
         optionLenses.textContent = element.lenses[i];
     }
 
+    let initialisation = document.querySelector(".form-control").value = 1 ;
+    let moins = document.getElementsByClassName('moins') ;
+    let plus = document.getElementsByClassName('plus') ;
     
-
-
     
-    // `<div  class="col d-flex justify-content-center">
-    //     <div class="card " style="width: 18rem;">
-    //         <p class="align-self-center">  ${element.name}  </p>  
-    //         <img width="100%" src='  ${element.imageUrl}  '>
-    //         <div class="card-body text-center">
-    //             <p class="text">   ${element.price/100} <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-currency-euro" viewBox="0 0 16 16">
-    //             <path d="M4 9.42h1.063C5.4 12.323 7.317 14 10.34 14c.622 0 1.167-.068 1.659-.185v-1.3c-.484.119-1.045.17-1.659.17-2.1 0-3.455-1.198-3.775-3.264h4.017v-.928H6.497v-.936c0-.11 0-.219.008-.329h4.078v-.927H6.618c.388-1.898 1.719-2.985 3.723-2.985.614 0 1.175.05 1.659.177V2.194A6.617 6.617 0 0 0 10.341 2c-2.928 0-4.82 1.569-5.244 4.3H4v.928h1.01v1.265H4v.928z"/>
-    //           </svg>  </p>
-    //             <select class="drop">
-    //             </select>
-    //             <p>  ${element.description}  </p>
-    //             <button type="button" class="btn btn-primary "> <a href="basket.html?${element._id}" >Ajouter</a> </button>
-    //         </div>
-    //     </div>
-    // </div>`
-
-    
-    let result = document.querySelector("#resultat")
-     res = result.value;
-    plus.addEventListener('click', function(){
-        if (result >= 1 && result < 20 ) {
-            result++;
-            // document.getElementById('resultat').value = result;
-        }
-    });
+    // let result = document.querySelector("#resultat")
+    //  res = result.value;
     moins.addEventListener('click', function(){
         if (result > 1 && result <= 20 ) {
             result--;
             // document.getElementById('resultat').value = result;
         }
-        console.log(result);
 
+    });
+
+    plus.addEventListener('click', function(){
+        if (result >= 1 && result < 20 ) {
+            result++;
+            // document.getElementById('resultat').value = result;
+        }
     });
     
 
