3a67e5886de08dc18a099fe2a1d22354e6d69ea7
Zidiny Erre <mr.erre.zidiny@gmail.com>
2021-08-06 18:58:16 +0200

    Essaie de changement de position

--- customization.js
+++ customization.js
@@ -11,37 +11,49 @@ fetch("http://localhost:3000/api/cameras/" + id)
 
 const recover = (element) => {
     console.log(element);
-    // document.querySelector(".row").innerHTML +=
+    // carte Bootstrap
     let card = document.querySelector(".card");
+    
+    // Création et mise en place de la photo
+    let imgAppareil = document.createElement("img");
+    imgAppareil.classList.add("imgAppareil");
+    imgAppareil.setAttribute('src', element.imageUrl);
+    card.prepend(imgAppareil);
 
+    // Création et mise en place du nom du produit
     let name = document.createElement("h2");
     name.classList.add("name");
     name.classList.add("align-self-center");
     name.innerHTML = element.name;
-    card.appendChild(name);
+    card.prepend(name);
 
-    let imgAppareil = document.createElement("img");
-    imgAppareil.classList.add("imgAppareil");
-    imgAppareil.setAttribute('src', element.imageUrl);
-    card.appendChild(imgAppareil);
-    
+    // Création et mise en place du prix
     let price = document.createElement("p");
     price.classList.add("montant");
     price.classList.add("align-self-center");
     price.innerHTML = element.price/100+".00" + '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-currency-euro" viewBox="0 0 16 16"><path d="M4 9.42h1.063C5.4 12.323 7.317 14 10.34 14c.622 0 1.167-.068 1.659-.185v-1.3c-.484.119-1.045.17-1.659.17-2.1 0-3.455-1.198-3.775-3.264h4.017v-.928H6.497v-.936c0-.11 0-.219.008-.329h4.078v-.927H6.618c.388-1.898 1.719-2.985 3.723-2.985.614 0 1.175.05 1.659.177V2.194A6.617 6.617 0 0 0 10.341 2c-2.928 0-4.82 1.569-5.244 4.3H4v.928h1.01v1.265H4v.928z"/></svg>';
-    card.appendChild(price);
+    card.append(price);
 
-    // Définition de la premier option de focale
+    // Création et mise en place du select
     let select = document.createElement("select");
     select.classList.add(".drop");
-    card.appendChild(select);
+    card.append(select);
 
+    // Création et mise en place du texte
+    let description = document.createElement("p");
+    description.classList.add("text");
+    description.classList.add("text-center");
+    description.innerHTML = element.description;
+    card.append(description);
+
+    // Définition de la premier option de focale
     let firstOption = document.createElement("option");
     firstOption.setAttribute('disabled', "disabled");
     firstOption.setAttribute('value', "0");
     firstOption.setAttribute('selected', "true");
     firstOption.textContent = "Choisissez la focale";
-    select.appendChild(firstOption);
+    select.prepend(firstOption);
+
     // Définition des autres options de focale
     for( i = 0; i<element.lenses.length; i++){
         let optionLenses = document.createElement("option");
@@ -51,17 +63,8 @@ const recover = (element) => {
         optionLenses.textContent = element.lenses[i];
     }
 
-    let description = document.createElement("p");
-    description.classList.add("text");
-    description.classList.add("text-center");
-    description.innerHTML = element.description;
-    card.appendChild(description);
-
-
-
-
-
     
+
 
     
     // `<div  class="col d-flex justify-content-center">
@@ -81,19 +84,6 @@ const recover = (element) => {
     // </div>`
 
     
-    
-
-    // Création des boutons - et +
-    // let paragraphe = document.querySelector(".text");
-    // let plus = document.createElement("button");
-    // plus.classList.add("plus");
-    // plus.textContent = "+";
-    // paragraphe.appendChild(plus);
-    // let moins = document.createElement("button");
-    // moins.classList.add("moins");
-    // moins.textContent = "-";
-    // paragraphe.appendChild(moins);
-    // Action du clique pour les boutons + et -
     let result = document.querySelector("#resultat")
      res = result.value;
     plus.addEventListener('click', function(){
