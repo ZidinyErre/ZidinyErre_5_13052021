7dcbf2375c8ddea31063c8a5f8d5b0e41a4dc7f1
Zidiny Erre <mr.erre.zidiny@gmail.com>
2021-08-11 18:21:35 +0200

    Réussite de mise en page de la card

--- customization.js
+++ customization.js
@@ -13,38 +13,24 @@ const recover = (element) => {
     console.log(element);
     // carte Bootstrap
     let card = document.querySelector(".card");
-
-    // Création et mise en place de la photo
-    let imgAppareil = document.createElement("img");
-    imgAppareil.classList.add("imgAppareil");
-    imgAppareil.setAttribute('src', element.imageUrl);
-    card.prepend(imgAppareil);
+    let focale = document.querySelector(".focale");
 
     // Création et mise en place du nom du produit
-    let name = document.createElement("h2");
-    name.classList.add("name");
-    name.classList.add("align-self-center");
+    let name = document.querySelector(".name");
     name.innerHTML = element.name;
-    card.prepend(name);
+    
+    // Création et mise en place de la photo
+    let imgAppareil = document.querySelector(".img");
+    imgAppareil.setAttribute('src', element.imageUrl);
 
     // Création et mise en place du prix
-    let price = document.createElement("p");
-    price.classList.add("montant");
-    price.classList.add("align-self-center");
+    let price = document.querySelector(".montant");
     price.innerHTML = element.price/100+".00" + '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-currency-euro" viewBox="0 0 16 16"><path d="M4 9.42h1.063C5.4 12.323 7.317 14 10.34 14c.622 0 1.167-.068 1.659-.185v-1.3c-.484.119-1.045.17-1.659.17-2.1 0-3.455-1.198-3.775-3.264h4.017v-.928H6.497v-.936c0-.11 0-.219.008-.329h4.078v-.927H6.618c.388-1.898 1.719-2.985 3.723-2.985.614 0 1.175.05 1.659.177V2.194A6.617 6.617 0 0 0 10.341 2c-2.928 0-4.82 1.569-5.244 4.3H4v.928h1.01v1.265H4v.928z"/></svg>';
-    card.append(price);
 
     // Création et mise en place du select
     let select = document.createElement("select");
     select.classList.add("drop");
-    card.append(select);
-
-    // Création et mise en place du texte
-    let description = document.createElement("p");
-    description.classList.add("text");
-    description.classList.add("text-center");
-    description.innerHTML = element.description;
-    card.append(description);
+    focale.appendChild(select);
 
     // Définition de la premier option de focale
     let firstOption = document.createElement("option");
@@ -52,7 +38,7 @@ const recover = (element) => {
     firstOption.setAttribute('value', "0");
     firstOption.setAttribute('selected', "true");
     firstOption.textContent = "Choisissez la focale";
-    select.prepend(firstOption);
+    select.appendChild(firstOption);
 
     // Définition des autres options de focale
     for( i = 0; i<element.lenses.length; i++){
@@ -62,6 +48,13 @@ const recover = (element) => {
         optionLenses.setAttribute('value', "1");
         optionLenses.textContent = element.lenses[i];
     }
+
+
+    // Création et mise en place du texte
+    let description = document.querySelector(".texte");
+    description.innerHTML = element.description;
+
+    
 
     // Ici on crée les variables qui vise l'input et les deux boutons et on crée une variable avec la fonction parsInt pour rendre que des nombres décimales
     let init = document.querySelector(".form-control").value = 1 ;
